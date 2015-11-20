32ca6a060fa239b30d311ae7856917ea77d45c18
José Andy Quintero Melo <andy.qmelo@udc.es>
2015-11-20 17:50:04 +0100

    Pull request atendida. #5

--- src/main/java/es/udc/fi/PracticaVVS/contenidos/Cancion.java
+++ src/main/java/es/udc/fi/PracticaVVS/contenidos/Cancion.java
@@ -3,26 +3,26 @@ package es.udc.fi.PracticaVVS.contenidos;
 import java.util.ArrayList;
 import java.util.List;
 
-import es.udc.fi.PracticaVVS.utiles.DuracionErroneaCancionException;
+import es.udc.fi.PracticaVVS.utiles.UnexistingContenidoException;
 
 public class Cancion extends ContenidoSimple {
 
-	public Cancion(String titulo, long duracion)
-			throws DuracionErroneaCancionException {
-		super(titulo, Math.abs(duracion));
-		if (duracion == 0) {
-			throw new DuracionErroneaCancionException();
-		}
+	public Cancion(String titulo, long duracion) {
+		super(titulo, duracion);
 	}
 
 	public List<Contenido> buscar(String subcadena) {
-		return null;
+		ArrayList<Contenido> lista = new ArrayList<Contenido>();
+                if (super.obtenerTitulo().contains(subcadena)){
+			lista.add(this);
+		}
+		return lista;
 	}
 
 	public void agregar(Contenido contenidoSimple, Contenido predecesor) {
 	}
 
-	public void eliminar(Contenido contenidoSimple) {
+	public void eliminar(Contenido contenidoSimple) {		
 	}
 
 	public List<Contenido> obtenerListaReproduccion() {
@@ -30,5 +30,5 @@ public class Cancion extends ContenidoSimple {
 		lista.add(this);
 		return lista;
 	}
-
+	
 }
